da15209884c5a4eb3196a9df7d264f4bcceb6b0b
Aisha Noori <aisha.noori2000@gmail.com>
2022-06-23 14:05:42 +0200

    create base backend

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'; 
+import dotenv from 'dotenv'; 
+import mongoose from 'mongoose'; 
+import cors from 'cors'; 
+import router from "./router/index.js"; 
+
+dotenv.config()
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(process.env.PORT, () => {
+    console.log("app listen to port ", process.env.PORT);
+});
+
+
+mongoose
+  .connect("mongodb://localhost:27017/musicApp")
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => console.error(err));

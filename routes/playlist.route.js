49cdd4124abd0e57583da7727a77b3622376b3c4
Aisha Noori <aisha.noori2000@gmail.com>
2022-06-29 09:28:37 +0200

    added alittle content in controller

--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -4,7 +4,26 @@ import { Router } from "express";
 
 
 
-const router =Router()
+const router = Router()
+
+router.get("/getAllPlaylists", PlaylistController.getAllPlaylist)
+router.get("/getPlaylistById/:id", PlaylistController.getPlaylistById)
+router.post("/addPlaylist",(()=>{}))
+router.put("/addSong/:playlistId", ()=>{})
+router.put("/rename/:playlistId",()=>{})
+router.delete("/deletePlaylist/:id",()=>{})
+router.delete("/deleteSongFromPlaylist/playlistId/:songId", () => { })
+router.post("/addComment",()=>{})
+router.delete("/deleteComment/:playlistId/:id", () => { })
+router.put("/updateComment/:playlistId/:id", () => { });
+router.get("/search", () => { })
+router.get("/like/:playlistId", () => { })
+router.get("/disLike/:playlistId", () => {});
+
+
+
+
+
 
 
 

0bd4b9f33ee8af2fe5f97ed20fdffdfc1886de9a
Aisha Noori <aisha.noori2000@gmail.com>
2022-07-07 12:36:29 +0200

    addfavorit

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-
-
+import { addPlaylistToFavorite, getFavoritePlaylists, removePlaylistFromFavorite} from "../controller/user.controller.js";
+import { auth } from "../middleware/auth.js";
 const router = Router();
 
-router.put("/editProfile", () => {});
-router.get("/addPlaylistToFavorite/:playlistId", () => {});
-router.put("/editProfile", () => {});
-router.get("/getMyPlaylists", () => {});
+router.get("/addPlaylistToFavorite/:playlistId", auth, addPlaylistToFavorite);
+router.get("/getFavoritePlaylists", auth, getFavoritePlaylists);
+router.delete("/removePlaylistFromFavorite/:playlistId", auth, removePlaylistFromFavorite);
+
+//router.put("/editProfile", () => {});
+//router.put("/editProfile", () => {});
 
 export default router;

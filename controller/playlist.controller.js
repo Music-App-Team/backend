5480a4734c8ec32b888aa5f80bbd307f3c5b2bac
Eli <89070187+aisha2000-hub@users.noreply.github.com>
2022-07-13 13:22:40 +0200

    Eli :done addSong and getDetail
    * addfavorit
    
    * wip
    
    * done profileImage in backend
    
    * done in backend addSong and getdetail

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -1,8 +1,9 @@
 import { PlayList } from "../model/playlist.model.js";
+import { User } from "../model/user.model.js";
+
 
 
 export const getAllPlaylist = async (req, res) => {
-
   const playlists = await PlayList.find().populate("user", ["firstName"]);
   res.send(playlists);
 };
@@ -15,19 +16,64 @@ export const getPlaylist = async (req, res) => {
 
 
 
-export const addPlaylist = async (req, res) => {
 
+export const addPlaylist = async (req, res) => {
   const { title } = req.body;
-  if (!title)
-    return res.status(400).send({ message: "title required" });
+  if (!title) return res.status(400).send({ message: "title required" });
   let playlist = new PlayList({
     title,
     user: req.user.id,
   });
-
   playlist = await playlist.save();
+  res.send(playlist);
+};
+
+
+
+export const getDetail = async (req, res) => {
+  const playlistId = req.params.playlistId;
+  const userId = req.user.id;
+
+  const playlist = await PlayList.findById(playlistId).populate("user", [
+    "firstName",
+  ]);
+  if (!playlist)
+    return res.status(404).send({ message: "play list with this id not found" });
+  res.send({ playlist, own: playlist.user._id.toString() === userId });
+};
+
+
+
+
+export const addSong = async (req, res) => {
+  const { name, artist, album, lang, link } = req.body;
+  const playlistId = req.params.playlistId;
+
+  const playlist = await PlayList.findById(playlistId);
+  if (!playlist) return res.status(400).send({ message: "playlist not found" });
+
+  playlist.songs.push({
+    name,
+    artist,
+    album,
+    lang,
+    link,
+  });
+
+  await playlist.save();
 
   res.send(playlist);
 };
+
+
+
+export const uploadSong = async (req, res) => {
+  const songPath = req.file?.path;
+
+  if (!songPath) return res.status(400).send({ message: "file must sent" });
+
+  res.send({ path: songPath });
+};
+
 
 
--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -1,11 +1,32 @@
 import { Router } from "express";
 import { addComment, getAllComments,deleteComment,getComment } from "../controller/comment.controller.js";
-import { getAllPlaylist,addPlaylist ,getPlaylist} from "../controller/playlist.controller.js";
+import { getAllPlaylist,addPlaylist,addSong ,getDetail, uploadSong} from "../controller/playlist.controller.js";
 import { auth } from "../middleware/auth.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "DEV",
+    resource_type: "auto",
+  },
+});
+
+const upload = multer({ storage });
+
 
 const router = Router();
 
 router.get("/getAllPlaylists",auth, getAllPlaylist);
+router.post("/addPlaylist", auth, addPlaylist);
+router.get("/getDetail/:playlistId", auth, getDetail);
+router.post("/addSong/:playlistId",auth, addSong);
+router.post("/uploadSong", [auth, upload.single("songFile")], uploadSong);
+
+//router.get("/getPlaylistById/:id",()=>{});
 router.post("/addPlaylist",auth, addPlaylist);
 //router.put("/addSong/:playlistId", () => {});
 router.get("/getPlaylistById/:id",auth,getPlaylist);

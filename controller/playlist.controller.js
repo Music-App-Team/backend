1bfc1dbac33dd25cd255c2e196b0a46111048d64
slavablack <veaceslav.ciornii@magenta.de>
2022-07-13 15:18:23 +0200

    wip

--- controller/playlist.controller.js
+++ controller/playlist.controller.js
@@ -1,18 +1,13 @@
 import { PlayList } from "../model/playlist.model.js";
 
-
 export const getAllPlaylist = async (req, res) => {
-
   const playlists = await PlayList.find().populate("user", ["firstName"]);
   res.send(playlists);
 };
 
-
 export const addPlaylist = async (req, res) => {
-
   const { title } = req.body;
-  if (!title)
-    return res.status(400).send({ message: "title required" });
+  if (!title) return res.status(400).send({ message: "title required" });
   let playlist = new PlayList({
     title,
     user: req.user.id,
@@ -23,7 +18,15 @@ export const addPlaylist = async (req, res) => {
   res.send(playlist);
 };
 
-
-
-   
-
+export const search = async (req, res) => {
+  try {
+    const playlists = await PlayList.find({ title: req.query.title }).populate(
+      "user",
+      ["firstName"]
+    );
+    res.send(playlists);
+  } catch (err) {
+    console.log("message", err);
+    return res.status(500).send({ message: err.message });
+  }
+};
--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -1,23 +1,32 @@
 import { Router } from "express";
-import { addComment, getAllComments,deleteComment,getComment } from "../controller/comment.controller.js";
-import { getAllPlaylist,addPlaylist } from "../controller/playlist.controller.js";
+import {
+  addComment,
+  getAllComments,
+  deleteComment,
+  getComment,
+} from "../controller/comment.controller.js";
+import {
+  getAllPlaylist,
+  addPlaylist,
+  search,
+} from "../controller/playlist.controller.js";
 import { auth } from "../middleware/auth.js";
 
 const router = Router();
 
-router.get("/getAllPlaylists",auth, getAllPlaylist);
-router.post("/addPlaylist",auth, addPlaylist);
+router.get("/getAllPlaylists", auth, getAllPlaylist);
+router.post("/addPlaylist", auth, addPlaylist);
 //router.put("/addSong/:playlistId", () => {});
 //router.get("/getPlaylistById/:id",()=>{});
 //router.put("/rename/:playlistId", () => {});
 //router.delete("/deletePlaylist/:id", () => {});
 //router.delete("/deleteSongFromPlaylist/playlistId/:songId", () => {});
-router.post("/addComment", auth,addComment);
-router.get("/getAllComments",auth, getAllComments);
-router.get("/getComment/:id",auth, getComment);
-router.delete("/deleteComment/:id",auth, deleteComment);
+router.post("/addComment", auth, addComment);
+router.get("/getAllComments", auth, getAllComments);
+router.get("/getComment/:id", auth, getComment);
+router.delete("/deleteComment/:id", auth, deleteComment);
 //router.put("/updateComment/:playlistId/:id", () => {});
-//router.get("/search", () => {});
+router.get("/search", auth, search);
 //router.get("/like/:playlistId", () => {});
 //router.get("/disLike/:playlistId", () => {});
 
